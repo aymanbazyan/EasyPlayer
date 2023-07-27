2160a98290992e681ffb409ab5dd26bba1c61835
Ayman <aymanbazyan@gmail.com>
2023-07-27 15:56:55 +0300

    Added auidos list

--- src/scripts/Views/View.js
+++ src/scripts/Views/View.js
@@ -1,2 +1,50 @@
-export default class View {}
+export default class View {
+  _data;
+
+  render(data) {
+    if (!data || (Array.isArray(data) && !data.length)) return;
+
+    this._data = data;
+    const markup = this._generateMarkup(this._data);
+
+    this.clear();
+    this._parentEl.insertAdjacentHTML("beforeend", markup);
+  }
+
+  update(data) {
+    this._data = data;
+    const newMarkup = this._generateMarkup();
+
+    // 1) Create new virtual DOM
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
+
+    // 2) Select & save all new DOM elements
+    // Convert from node lists to arrays:
+    const curElements = Array.from(this._parentEl.querySelectorAll("*"));
+    const newElements = Array.from(newDOM.querySelectorAll("*"));
+    newElements.forEach((newEl, i) => {
+      // Compare curElements and newElements
+      const curEl = curElements[i];
+
+      // Update changed text
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ""
+      )
+        curEl.textContent = newEl.textContent;
+
+      // Update changed attributes
+      if (!newEl.isEqualNode(curEl)) {
+        Array.from(newEl.attributes).forEach((attr) =>
+          curEl.setAttribute(attr.name, attr.value)
+        );
+      }
+    });
+  }
+
+  clear() {
+    this._parentEl.innerHTML = "";
+  }
+}
+
 // export default new View();
---
+++ src/scripts/Views/listView.js
@@ -0,0 +1,86 @@
+import View from "./View.js";
+
+class ListView extends View {
+  _lists = document.querySelectorAll(".list");
+  _listHeaders = document.querySelectorAll(".list__header");
+  _parentEl = document.querySelector(".list__audios");
+  _tabeHeader = document.querySelector(".tabe-header");
+  _radioTitle = document.querySelector(".tabe-title");
+  _listItems;
+
+  toggleList(hdr = this._tabeHeader) {
+    const arr = hdr.querySelector(".list__header-arrow");
+    const list = hdr.nextElementSibling;
+
+    list.classList.toggle("list-hidden");
+    arr.classList.toggle("fa-rotate-270");
+  }
+
+  openList(hdr = this._tabeHeader) {
+    const arr = hdr.querySelector(".list__header-arrow");
+    const list = hdr.nextElementSibling;
+
+    list.classList.remove("list-hidden");
+    arr.classList.add("fa-rotate-270");
+  }
+
+  updateTitle(title = "Please pick a tabe!") {
+    const markup = `
+    ${title}
+    <i class="fa-solid fa-music"></i>
+    `;
+    this._radioTitle.innerHTML = markup;
+  }
+
+  _generateMarkup() {
+    return this._data.map(this._generateMarkupPreview).join("");
+  }
+
+  _generateMarkupPreview(data) {
+    return `
+    <li class="list__audios-item" id="${data.name}">
+    ${data.name} -
+    ${data.duration}
+    <i class="fa-solid fa-${
+      data.selected ? "pause" : "play"
+    } PlayPauseIcon"></i>
+  </li>
+   `;
+  }
+
+  updateListItemsSelector() {
+    this._listItems = document.querySelectorAll(".list__audios-item");
+  }
+
+  updatePlayPauseIcon(curItem) {
+    this._listItems.forEach((item) => {
+      item.querySelector(".PlayPauseIcon").classList.add("fa-play");
+      item.querySelector(".PlayPauseIcon").classList.remove("fa-pause");
+    });
+
+    curItem.querySelector(".PlayPauseIcon").classList.remove("fa-play");
+    curItem.querySelector(".PlayPauseIcon").classList.add("fa-pause");
+  }
+
+  listItemsHandler(handler) {
+    this._lists.forEach((list) => {
+      list.addEventListener("click", (e) => {
+        const curItem = e.target.closest(".list__audios-item");
+        if (!curItem) return;
+
+        handler(curItem);
+      });
+    });
+  }
+
+  listHeaderHandler(handler) {
+    this._listHeaders.forEach((header) => {
+      header.addEventListener("click", (e) => {
+        const hdr = e.target.closest(".list__header");
+        handler(hdr);
+      });
+    });
+  }
+}
+
+export default new ListView();
